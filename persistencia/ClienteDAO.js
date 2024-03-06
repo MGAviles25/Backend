061904f22a5b17db912e3f7a40af0385d61237ee
MGAviles25 <160518131+MGAviles25@users.noreply.github.com>
2024-03-06 15:40:33 +0000

    cc

--- persistencia/ClienteDAO.js
+++ persistencia/ClienteDAO.js
@@ -1,11 +1,33 @@
-import conectar from "./Conexão.js";
+import conectar from "./Conexão.js"; 
 import Cliente from "../modelo/Ciente.js";
-export default class ClienteDAO{
 
+export default class ClienteDAO{
     async gravar(cliente){
-        if(cliente instanceof Cliente){  
+        if (cliente instanceof Cliente){
             const conexao = await conectar();
-            const sql = 'INSERT INTO cliente (cpf, nome, dataNasc, telefone, email, cidade) values (?, ?, ?, ?, ?, ?)';
+            const sql = `INSERT INTO cliente (cpf, nome, dataNasc, telefone, email,
+                         cidade) 
+                         values (?, ?, ?, ?, ?, ?)`;
+            const parametros = [
+                cliente.cpf,
+                cliente.nome,
+                cliente.dataNasc,
+                cliente.telefone,
+                cliente.email,
+                cliente.cidade 
+            ];
+            const [resultados, campos] = await conexao.execute(sql,parametros);
+            
+            cliente.codigo = resultados.insertId; 
+        }
+    }
+
+    async atualizar(cliente){
+        if (cliente instanceof Cliente){
+            const conexao = await conectar();
+            const sql = `UPDATE cliente SET cpf = ?,
+                         nome = ?, dataNasc = ?, telefone = ?,
+                         email = ?, cidade = ? WHERE id = ?`;
             const parametros = [
                 cliente.cpf,
                 cliente.nome,
@@ -13,74 +35,53 @@ export default class ClienteDAO{
                 cliente.telefone,
                 cliente.email,
                 cliente.cidade
-
-            ];
-            const[resultados, campos] = await conexao.execute(sql,parametros);
-            cliente.codigo = resultados.insertId
-        }
-    }
-
-    async atualizar(cliente){
-        if(cliente instanceof Cliente){
-            const conexao = await conectar();
-            const sql = `UPDATE cliente SET cpf = ?, nome = ?, dataNasc = ?, telefone = ?, email = ?, cidade= ? WHERE codigo = ?`;
-            const parametros = [
-                cliente.cpf,
-                cliente.nome,
-                cliente.dataNasc,
-                cliente.telefone,
-                cliente.email,
-                cliente.cidade 
-
             ];
 
             await conexao.execute(sql,parametros);
         }
     }
 
-    async excluir(cliente){   
-        if(cliente instanceof Cliente){
+    async excluir(cliente){
+        if (cliente instanceof Cliente){
             const conexao = await conectar();
-            const sql = `DELETE FROM cliente WHERE codigo = ?`;
+            const sql = `DELETE FROM cliente WHERE id = ?`;
             const parametros = [
                 cliente.codigo
             ]
             await conexao.execute(sql,parametros);
         }
-
     }
 
+   
+    
     async consultar(termoDePesquisa){
-        if ( termoDePesquisa === undefined){
+        if (termoDePesquisa === undefined){
             termoDePesquisa = "";
         }
         let sql="";
-        if (isNaN(termoDePesquisa)){
-            sql = `SELECT * FROM cliente WHERE nome LIKE '%?%'`;
-        
+        if (isNaN(termoDePesquisa)){ 
+            sql = `SELECT * FROM cliente WHERE nome LIKE ?`;
+            termoDePesquisa= '%' + termoDePesquisa + '%';
         }
         else{
-            sql = `SELECT * FROM cliente WHERE codigo = ?`;
+            sql = `SELECT * FROM cliente WHERE id = ?`;
         }
 
         const conexao = await conectar();
-        const [registro] = await conexao.execute(sql,[termoDePesquisa]);
-
-        let listaCliente = [];
+        const [registros] = await conexao.execute(sql,[termoDePesquisa]);
+       
+        let listaClientes = [];
         for (const registro of registros){
-            const cliente = new Cliente(  
+            const cliente = new Cliente(
                 cliente.cpf,
                 cliente.nome,
                 cliente.dataNasc,
                 cliente.telefone,
                 cliente.email,
-                cliente.cidade 
-
+                cliente.cidade
             );
-            listaCliente.push(cliente)
-
+            listaClientes.push(cliente);
         }
-        return listaCliente
+        return listaClientes;
     }
-
 }
