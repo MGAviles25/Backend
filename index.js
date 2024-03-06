3426ede8e7571e2c620988b96f54ed06acd584e7
MGAviles25 <160518131+MGAviles25@users.noreply.github.com>
2024-03-06 15:43:26 +0000

    ssd

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import Cliente from "./modelo/Ciente.js";
 const cliente = new Cliente(0,"122.435.234.32", "marcos", "12/12/2012", "(12)99999-0000", "marco@ip.com", "SP");
 
 
-cliente.gravar().then(() => {
-    console.log("cliente gravado com sucesso!");
+const clienteQQ = new Cliente();
 
+clienteQQ.consultar(3).then((listaClientes) => {
+    console.log("Clientes encontrados:")
+    for (const cliente of listaClientes) {
+        console.log(cliente.toJSON());
+    }
 }).catch((erro) => {
-    console.log(erro.message)
-
+    console.log("Não foi possível consultar o cliente", erro);
 });
