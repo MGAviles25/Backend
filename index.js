e230bc81aa708e25f00bd3edecbcd150556bf16f
MGAviles25 <160518131+MGAviles25@users.noreply.github.com>
2024-03-06 18:29:47 +0000

    x

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Cliente from "./modelo/Cliente.js";
 
 
-const cliente = new Cliente(0,"122.435.234.32", "marcos", "12/12/2012", "(12)99999-0000", "marco@ip.com", "SP");
+const cliente = new Cliente(1,"122.435.234.32", "marcos", "12/12/2012", "(12)99999-0000", "marco@ip.com", "SP");
 
 
 cliente.gravar().then(() => {
