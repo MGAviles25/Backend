22d6e5cc383a9a529ab81d30b10445f8a7864308
MGAviles25 <160518131+MGAviles25@users.noreply.github.com>
2024-03-06 23:42:07 +0000

    vbgh

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ import Evento from "./modelo/Evento.js";
 
 const evento = new Evento(3, "143.432.875-10", "marcos", "10/12/2010", "(10)32131-3313", "marco@ig.com", "parapuã");
 
+console.log(evento.toString());
+console.log(evento.toJSON());
 
-evento.gravar().then(() =>{
-    console.log("participante gravado com sucesso!");
-}).catch((erro) => {
-    console.log(erro);
-});
