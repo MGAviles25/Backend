01122960359754148bc6349f1ee6cea01c6a8ac7
MGAviles25 <160518131+MGAviles25@users.noreply.github.com>
2024-03-06 02:11:28 +0000

    fm

--- persistencia/ClienteDAO.js
+++ persistencia/ClienteDAO.js
@@ -1,20 +1,70 @@
 import conectar from "./Conexão.js";
-
+import Cliente from "../modelo/Ciente.js";
 export default class ClienteDAO{
 
-    gravar(cliente){
+    async gravar(cliente){
+        if(cliente instanceof Cliente){  
+            const conexão = await conectar();
+            const sql = 'INSERT INTO cliente (cpf, nome, dataNasc, telefone, email, cidade) values (?, ?, ?, ?, ?, ?)';
+            const parametros = [
+                cliente.cpf,
+                cliente.nome,
+                cliente.dataNasc,
+                cliente.telefone,
+                cliente.email,
+                cliente.cidade
+
+            ];
+            const[resultados, campos] = await conexao.execute(sql,parametros);
+            cliente.codigo = resultados.insertId
+        }
+    }
+
+    async atualizar(cliente){
+        if(cliente instanceof Cliente){
+            const conexão = await conectar();
+            const sql = `UPDATE cliente SET cpf = ?, nome = ?, dataNasc = ?, telefone = ?, email = ?, cidade= ? WHERE codigo = ?`;
+            const parametros = [
+                cliente.cpf,
+                cliente.nome,
+                cliente.dataNasc,
+                cliente.telefone,
+                cliente.email,
+                cliente.cidade 
+
+            ];
+
+            await conexao.execute(sql,parametros);
+        }
+    }
+
+    async excluir(cliente){   
+        if(cliente instanceof Cliente){
+            const conexao = await conectar();
+            const sql = `DELETE FROM cliente WHERE codigo = ?`;
+            const parametros = [
+                cliente.codigo
+            ]
+            await conexao.execute(sql,parametros);
+        }
+
+    }
+
+    async consultar(termoDePesquisa){
+        if ( termoDePesquisa === undefined){
+            termoDePesquisa = "";
+        }
+        let sql="";
+        if (isNaN(termoDePesquisa)){
+            sql = `SELECT * FROM cliente WHERE nome LIKE '%?%'`;
         
-    }
+        }
+        else{
+            sql = `SELECT * FROM cliente WHERE codigo = ?`;
+        }
 
-    atualizar(cliente){
-        
-    }
-
-    excluir(cliente){
-
-    }
-
-    consultar(termoDePesquisa){
+        const conexao = await conectar();
+        const [registro] = await conexao.execute(sql,[termoDePesquisa]);
 
     }
 
