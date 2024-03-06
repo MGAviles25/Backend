65492f751fab64b398397c99d2b4245806cf8a03
MGAviles25 <160518131+MGAviles25@users.noreply.github.com>
2024-03-06 13:08:39 +0000

    dsa

---
+++ index.js
@@ -0,0 +1,12 @@
+import Cliente from "./modelo/Ciente.js";
+
+
+const cliente = new Cliente(codigo = 0,
+cpf = "567.324.123.25",
+nome = "Marco Newgate",
+dataNasc ="21/03/1998",
+telefone ="(18) 99724-4082",
+email ="Marco.N55@ig.com",
+cidade ="");
+
+console.log(cliente);
